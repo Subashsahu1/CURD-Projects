c145effa6c9f672873044d2dfc46eeec35c58cb1
Omprakash sahu <subassahu72@gmail.com>
2022-02-01 10:11:54 +0530

    commit

---
+++ curdapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.db import models
+
+# Create your models here.
+
+class StudentsData(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    course_name = models.CharField(max_length=100)
+    fee = models.IntegerField()
+    assignment1 = models.IntegerField()
+    assignment2 = models.IntegerField()
+    assignment3 = models.IntegerField()
+    assignment4 = models.IntegerField()
+
